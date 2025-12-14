f93cd7c6748ecdbe181c8d62697be15bb3c65cd8
a1t2s2u2 <a1t2s2u2@icloud.com>
2025-12-15 01:41:19 +0900

    fix: モジュールの更新

--- rim/core/__init__.py
+++ rim/core/__init__.py
@@ -4,13 +4,10 @@ Tensor、自動微分、基本演算を提供
 """
 
 from .tensor import Tensor
-from .function import Function, Context
 from .activations import relu, sigmoid, tanh, exp, log, softmax
 
 __all__ = [
     'Tensor',
-    'Function',
-    'Context',
     'relu',
     'sigmoid',
     'tanh',
