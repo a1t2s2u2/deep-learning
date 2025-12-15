13cd07e2b6d0e607feb4d3ab3fdfd68243ab8492
a1t2s2u2 <a1t2s2u2@icloud.com>
2025-12-15 17:08:06 +0900

    feat: コアモジュールの更新

--- rim/core/__init__.py
+++ rim/core/__init__.py
@@ -4,10 +4,12 @@ Tensor、自動微分、基本演算を提供
 """
 
 from .tensor import Tensor
+from .parameter import Parameter
 from .activations import relu, sigmoid, tanh, exp, log, softmax
 
 __all__ = [
     'Tensor',
+    'Parameter',
     'relu',
     'sigmoid',
     'tanh',
